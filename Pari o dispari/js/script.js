28553792e9eea8ffde228f8b5665ed895869e471
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-21 15:50:20 +0200

    fine scrittura html e js

--- Pari o dispari/js/script.js
+++ Pari o dispari/js/script.js
@@ -2,16 +2,16 @@
 
 Pari e Dispari
 
-1. L'utente sceglie pari o dispari
-2. inserisce un numero da 1 a 5.
-3. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-4. Sommiamo i due numeri
-5. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-6. Dichiariamo chi ha vinto.
+1. L'utente sceglie pari o dispari - DONE
+2. inserisce un numero da 1 a 5 - DONE
+3. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione) - DONE, ma non bene ):
+4. Sommiamo i due numeri - DONE
+5. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) - DONE, ma non bene ):
+6. Dichiariamo chi ha vinto - DONE
 
 */
 
-// dichiaro il bottone pari
+// dichiaro il bottone PARI
 const PariButton = document.getElementById('pari');
 
 // ascolto il click sul bottone
@@ -35,5 +35,69 @@ PariButton.addEventListener("click", function () {
         const numUtente = parseInt(numInput.value);
         console.log('numUtente', numUtente, typeof numUtente);
 
+        // Genero un numero random
+        let x = Math.floor((Math.random() * 5) + 1);
+        console.log('x', x, typeof x)
+
+        // Sommo il numero dell'utente + il numero generato dal pc
+        let sum = parseInt(numUtente + x)
+        console.log('sum', sum, typeof sum)
+
+        // uso una funzione per decretare se il numero finale sia pari o dispari
+        function numeroFinale() {
+            if (sum % 2 == 0) {
+
+                // comunico in pagina se l'utente ha vinto
+                document.getElementById("risultato").innerText = 'Il numero che ho genera to è: ' + x + '. La somma è: ' + sum + '. Complimenti, hai vinto!';
+                return true;
+            }
+            else {
+
+                // comunico in pagina se l'utente ha perso
+                document.getElementById("risultato").innerText = 'Il numero che ho genera to è: ' + x + '. La somma è: ' + sum + '. Hai perso, ritenta!';
+                return false;
+            }
+        };
+        console.log('Il numero ' + sum + ' è pari?', numeroFinale());
+    });
+});
+
+// dichiaro il bottone DISPARI
+const DispariButton = document.getElementById('dispari');
+
+// ascolto il click sul bottone
+DispariButton.addEventListener("click", function () {
+
+    const form = document.getElementById('myformDispari');
+    form.classList.remove('none');
+
+    form.addEventListener('submit', function (event) {
+
+        event.preventDefault();
+
+        const numInput = document.getElementById('numInputDispari')
+
+        const numUtente = parseInt(numInput.value);
+        console.log('numUtente', numUtente, typeof numUtente);
+
+        let x = Math.floor((Math.random() * 5) + 1);
+        console.log('x', x, typeof x)
+
+        let sum = parseInt(numUtente + x)
+        console.log('sum', sum, typeof sum)
+
+        function numeroFinale() {
+            if (sum % 2 != 0) {
+
+                document.getElementById("risultato").innerText = 'Il numero che ho genera to è: ' + x + '. La somma è: ' + sum + '. Complimenti, hai vinto!';
+                return true;
+            }
+            else {
+
+                document.getElementById("risultato").innerText = 'Il numero che ho genera to è: ' + x + '. La somma è: ' + sum + '. Hai perso, ritenta!';
+                return false;
+            }
+        };
+        console.log('Il numero ' + sum + ' è dispari?', numeroFinale());
     });
 });
