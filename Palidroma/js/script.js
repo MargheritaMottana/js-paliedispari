92c47b2bf509cfeb8508dd682fd28ec29f2ba94a
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-22 17:15:54 +0200

    Palindroma: fine scrittura html e js

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -7,10 +7,10 @@ Palidroma
 
 */
 
-//dichiaro il form
+// dichiaro il form
 const form = document.getElementById('myform');
 
-//ascolto il form
+// ascolto il form
 form.addEventListener('submit', function (event) {
     event.preventDefault()
 
@@ -19,14 +19,47 @@ form.addEventListener('submit', function (event) {
 
     // prendo e dichiaro il valore dell'input
     const word = wordInput.value;
-    console.log('word', word, typeof word);
+    console.log('word', word, word.length, typeof word);
 
-    // devo Convertire la stringa in un - char array - ???
-    // come faccio a controllare che le lettere siano il loro inverso? bella domanda
+    // richiamo la funzione
+    const risultato = checkReverse(word);
+
+    if (risultato) {
+        document.getElementById('risultato').innerText = "La tua parola è palindroma"
+    }
+    else {
+        document.getElementById('risultato').innerText = "La tua parola NON è palindroma"
+    }
 
     // creo una funzione per controllare che la parola sia palindroma
-    function myFunction(?, ?) {
 
-    }
+    /* 
+    per svingolarmi dalla variabile globale,
+    devo inserire tra nell'argomento, una parola che non sia tra le variabile globale.
+    In questo modo, quando la richiamo, inserisco il valore che le devo passare e che userà per funzionare.
+    */
+
+    function checkReverse(userWord) {
+
+        // vuota perché attende il valore dall'input
+        let reverseWord = '';
+
+
+        for (i = userWord.length - 1; i >= 0; i--) {
+            console.log(userWord[i]);
+
+            reverseWord += userWord[i];
+            // si può scrivere anche così...
+            // reverseuserWord = reverseuserWord + userWord[i];
+        }
+        console.log('reverseWord', reverseWord, reverseWord.length, typeof reverseWord);
+
+        if (userWord == reverseWord) {
+            return true;
+        }
+        else {
+            return false;
+        };
+    };
 
 });
